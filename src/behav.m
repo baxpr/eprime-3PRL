6725de92707deaa024258f9fcbfc964cd87eeaba
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-10-19 11:21:04 -0500

    behav

---
+++ src/behav.m
@@ -0,0 +1,20 @@
+eprime_csv = '../INPUTS_behav/eprime_test.csv';
+
+% Read eprime .csv after conversion from original .txt with
+% eprime_to_csv.py
+eprime = readtable(eprime_csv);
+
+% "choices" = ??
+% "outcome" = 0 (lose) or 1 (win)
+
+% hgf_softmax_mu3 is the main analysis code
+% its input comes from socialPRL_datastruc.m
+% which just renames subject-data.xlsx
+% which comes from cols G-J of HGF_Input sheet of ???_template.xlsx
+% which comes from
+%    input_vector column D of ???_template.xlsx
+%       where did that come from? Entered by hand based on Raw Data N?
+%    and Raw Data P.
+%
+% Raw Data N = left/middle/right ChosenColor
+% Raw Data P = win (1) / lose (0)
