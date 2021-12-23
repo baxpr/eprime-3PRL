46891f19ca80f4c538931d8b96d67b7bcf25fbe5
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-12-23 15:26:09 -0500

    fitting

--- matlab/src/hgf_fit.m
+++ matlab/src/hgf_fit.m
@@ -16,16 +16,44 @@
 %	);
 
 % Load our eprime trials report
-info = readtable('../../OUTPUTS/new/eprime_report.csv');
+info = readtable('../../OUTPUTS/old/eprime_report.csv');
 
-% Split by "easy" and "hard" based on fmri run
+% Split by "easy" and "hard" based on fmri run. We will assume the 10/50/90
+% decks in inds1 and the 20/40/80 decks in inds2.
 inds1 = ismember(info.Run,[1 2]);
 inds2 = ismember(info.Run,[3 4]);
 
+% Coding and fit for "easy". Default to NaN so fitModel will ignore
+% responses or trials with missing info.
+responses1 = nan(sum(inds1),1);
+responses1(strcmp(info.ChosenProb(inds1),'Deck10')) = 1;
+responses1(strcmp(info.ChosenProb(inds1),'Deck50')) = 2;
+responses1(strcmp(info.ChosenProb(inds1),'Deck90')) = 3;
+
+outcomes1 = nan(sum(inds1),1);
+outcomes1(strcmp(info.Outcome(inds1),'Lose')) = 0;
+outcomes1(strcmp(info.Outcome(inds1),'Win')) = 1;
+
 result1 = tapas_fitModel( ...
-	info.ChosenProb(inds1), ...
-	info.Outcome(inds1), ...
+	responses1, ...
+	outcomes1, ...
 	'tapas_hgf_ar1_binary_mab_config_1', ...
 	'tapas_softmax_mu3_config' ...
 	);
 
+% Coding and fit for "hard"
+responses2 = nan(sum(inds2),1);
+responses2(strcmp(info.ChosenProb(inds2),'Deck20')) = 1;
+responses2(strcmp(info.ChosenProb(inds2),'Deck40')) = 2;
+responses2(strcmp(info.ChosenProb(inds2),'Deck80')) = 3;
+
+outcomes2 = nan(sum(inds2),1);
+outcomes2(strcmp(info.Outcome(inds2),'Lose')) = 0;
+outcomes2(strcmp(info.Outcome(inds2),'Win')) = 1;
+
+result2 = tapas_fitModel( ...
+	responses2, ...
+	outcomes2, ...
+	'tapas_hgf_ar1_binary_mab_config_1', ...
+	'tapas_softmax_mu3_config' ...
+	);
